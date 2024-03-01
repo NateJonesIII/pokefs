af4bd7b3380d18a52cda13c5f2da9239595f9b9e
Nate Jones <15nate.jones@gmail.com>
2024-03-01 00:44:49 -0500

    Fixes to color scheme to match button/ auth

--- src/auth/Register.tsx
+++ src/auth/Register.tsx
@@ -34,7 +34,7 @@ const Register: React.FC<AuthProps> = ({
             <h2>Register:</h2>
             <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleRegister}>LesGo!</button>
+            <button onClick={handleRegister}>Register!</button>
         </div>
     )
 }
