42ec60fcd4e71f3ea77a17019e152f4cc4af289c
Nate Jones <15nate.jones@gmail.com>
2024-03-20 19:48:40 -0400

    added constants folder structure

---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+// keep value constant for availability flow tied to user team
+export const AVAILABLE_FLOW = 'available';
+// keep value constant for user flow to determine if user isLoggedIn
+export const USER_FLOW = 'user';
+// keep value constant for add button state if added to user team
+export const ADD_TEXT = 'add';
+// keep value constant pokemon item ownership
+export const OWNED = 'owned';
